66b1fadc63a69c67b24a27a2251afa0ba7359619
paweltar <paweltar@gmail.com>
2017-08-19 11:24:55 +0200

    Intersection overlay works

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,51 @@
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
 import $ from 'jquery';
-import 'aos/dist/aos.css';
-import AOS from 'aos'
-import uikitCss from 'uikit/dist/css/uikit.css'
-import animations from './animations'
+// import 'aos/dist/aos.css';
+// import AOS from 'aos'
+// import uikitCss from 'uikit/dist/css/uikit.css'
+// import animations from './animations'
+import fullpage from 'fullpage.js';
+import 'fullpage.js/dist/jquery.fullpage.css';
+import {TweenMax, Power2, TimelineLite} from "gsap";
 
 UIkit.use(Icons);
 
-$(window).on("load", function(){
-  animations();
+
+// function leaving() {
+//   const tl = new TimelineLite();
+//   // const header = $('.header');
+
+//   // TweenMax.to(header, 1, {opacity: 0, y: 50});
+
+//   console.log('im leaving');
+// }
+
+// function loaded() {
+//   // const header = $('.header');
+  
+//   // TweenMax.from(header, 1, {opacity: 0, y: 50});
+//   console.log('section loaded!');
+// }
+
+$(document).ready(function() {
+  const menu = $('#menu-overlay');
+  const navDots = $('#fp-nav ul li a span');
+
+	$('#fullpage').fullpage({
+    anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+    navigation: true,
+    navigationPosition: 'right',
+    normalScrollElements: '#menu-overlay',
+    onLeave: function(index, nextIndex, direction){
+      menu.addClass('menu-active');
+      navDots.addClass('dot-light');    
+    },
+    afterLoad: function(anchorLink, index){
+      setTimeout(function(){ 
+        menu.removeClass('menu-active');
+        navDots.removeClass('dot-light');
+      }, 1500);
+    },
+  });
 });
